96997c99285d77ef5edff2c1a2de418380af6bd4
Eliton da Rosa <elitondr@live.com>
2023-02-06 14:45:53 -0300

    Missões Finalizado

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import date from '../assets/svgs/date.svg';
+import flag from '../assets/svgs/flag.svg';
+import pinLocation from '../assets/svgs/pinLocation.svg';
 
 class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{name}</p>
-        <p data-testid="mission-year">{year}</p>
-        <p data-testid="mission-country">{country}</p>
-        <p data-testid="mission-destination">{destination}</p>
+      <div className="mission-card">
+        <div className="mission-name">
+          <p>{name}</p>
+        </div>
+        <hr />
+        <div className="mission-details">
+          <div className="mission-year">
+            <img src={ date } alt={ year } />
+            <p>{year}</p>
+          </div>
+          <div className="mission-country">
+            <img src={ flag } alt={ country } />
+            <p>{country}</p>
+          </div>
+          <div className="mission-destination">
+            <img src={ pinLocation } alt={ destination } />
+            <p>{destination}</p>
+          </div>
+        </div>
       </div>
     );
   }
